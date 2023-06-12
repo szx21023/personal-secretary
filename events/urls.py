649f6732f10d26564a8e26b0339be66601ccf457
Herry <szx21023@gmail.com>
2023-06-12 14:04:02 +0800

    api: create event

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("list", views.list_all, name="list"),
+    path("create", views.create_event, name="create_event"),
 ]
